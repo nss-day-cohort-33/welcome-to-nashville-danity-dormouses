887c1c060654abe7bbf064af844c9737659bc236
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-06-12 11:24:29 -0500

    fetch in place and beginnings of other dom stuff

--- scripts/parks.js
+++ scripts/parks.js
@@ -1,17 +1,18 @@
 
 
 let parksSearchInput = document.querySelector("#input-parks")
+
 document.querySelector("#button-parks").addEventListener("click", () => {
-  fetch(`https://data.nashville.gov/resource/74d7-b74t.json`)
+  return fetch(`https://data.nashville.gov/resource/74d7-b74t.json`)
   .then( parkData => parkData.json())
   .then( parks => {
-    document.querySelector("#results").innerHTML = ""
-    AddParkComponentToDom(createParkSearchComponent(parks), "results")
+    document.querySelector("#results").innerHTML = "";
+    AddParkComponentToDom(createParkSearchComponent(parks), "results");
   });
 });
 
 function AddParkComponentToDom(parkComponent, element) {
-  document.querySelector(`#${element}`).innerHTML += parkComponent
+  document.querySelector(`#${element}`).innerHTML += parkComponent;
 }
 
 function createParkSearchComponent(parkObj) {
