08beb586c77f0d204bbf9ab950c8b5c1b2725b17
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-06-11 21:21:46 -0500

    minor edit to index file to keep title from being replaced, work on parks.js

---
+++ scripts/parks.js
@@ -0,0 +1,22 @@
+
+
+let parksSearchInput = document.querySelector("#input-parks")
+document.querySelector("#button-parks").addEventListener("click", () => {
+  fetch(`https://data.nashville.gov/resource/74d7-b74t.json`)
+  .then( parkData => parkData.json())
+  .then( parks => {
+    document.querySelector("#results").innerHTML = ""
+    AddParkComponentToDom(createParkSearchComponent(parks), "results")
+  });
+});
+
+function AddParkComponentToDom(parkComponent, element) {
+  document.querySelector(`#${element}`).innerHTML += parkComponent
+}
+
+function createParkSearchComponent(parkObj) {
+  return `
+    <h3>${parkObj[0].park_name}</h3>
+    <button>Save to Itinerary</button>
+  `;
+}
