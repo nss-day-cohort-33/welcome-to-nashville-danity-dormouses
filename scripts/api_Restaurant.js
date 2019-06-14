2bbfb536db5dc2fb947753e55cbcbc51ceb0b17e
Michael Drew Palazola <mpalazola@hotmail.com>
2019-06-13 20:30:24 -0500

    a few changes

--- scripts/api_Restaurant.js
+++ scripts/api_Restaurant.js
@@ -4,7 +4,7 @@
 // }
 
 let uniqueRestId = 0
-let uniqueButtonId = 0 
+let uniqueRestButtonId = 0 
 
 let inputCuisineData = document.getElementById("input-restaurants")
 document.getElementById("button-restaurants").addEventListener("click", () => {
@@ -14,7 +14,7 @@ document.getElementById("button-restaurants").addEventListener("click", () => {
     .then(food => {
         document.getElementById("results").innerHTML = "";
       for (let i = 0; i <food.restaurants.length ; i++) {
-          uniqueButtonId++ 
+          uniqueRestButtonId++ 
           uniqueRestId++
         AddFoodComponentToDom(createFoodSearchComponent(food.restaurants[i]))
         
@@ -43,12 +43,12 @@ function AddFoodComponentToDom(nameOfRestaurant) {
 function createFoodSearchComponent(foodObj) {
     return `
         <h2 id = rest-${uniqueRestId}>${foodObj.restaurant.name}</h2>
-            <button id = butt-${uniqueButtonId}>Save</button>
+            <button id = butt-${uniqueRestButtonId}>Save</button>
     `;
 } 
 
 function putMyRestaurantIntoTheItinerary (restaurantElement) {
-    document.getElementById("restaurant-itinerary").innerHTML = "Restaurant:" + restaurantElement;
+    document.getElementById("restaurant-itinerary").innerHTML = "Restaurant: " + restaurantElement;
     }
 
 
